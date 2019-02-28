c18c86aa59da13d1cb2f7b5d97a3055c03b4d298
zyhbili <1372241206@qq.com>
2019-02-28 11:01:07 +0800

    file

--- src/main/java/com/example/demo/controller/FileController.java
+++ src/main/java/com/example/demo/controller/FileController.java
@@ -1,6 +1,11 @@
 package com.example.demo.controller;
 
 
+import com.example.demo.domain.User;
+import com.example.demo.utils.Result;
+import com.example.demo.utils.ResultFactory;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,14 +19,16 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 @RestController
-@RequestMapping("/invitations")
+@RequestMapping("/files")
 @CrossOrigin
 public class FileController {
-    @RequestMapping(value = "/testUploadFile", method = RequestMethod.POST.POST)
-    public void testUploadFile(HttpServletRequest req,
-                               MultipartHttpServletRequest multiReq) {
+    @RequestMapping(value = "/test", method = RequestMethod.POST)
+    public Result testUploadFile(MultipartHttpServletRequest multiReq) {
+        Subject subject = SecurityUtils.getSubject();
+        User user = (User) subject.getPrincipal();
+
         // 获取上传文件的路径
-        String uploadFilePath = multiReq.getFile("file1").getOriginalFilename();
+        String uploadFilePath = multiReq.getFile("file").getOriginalFilename();
         System.out.println("uploadFlePath:" + uploadFilePath);
         // 截取上传文件的文件名
         String uploadFileName = uploadFilePath.substring(
@@ -33,9 +40,10 @@ public class FileController {
         System.out.println("uploadFileSuffix:" + uploadFileSuffix);
         FileOutputStream fos = null;
         FileInputStream fis = null;
+
         try {
-            fis = (FileInputStream) multiReq.getFile("file1").getInputStream();
-            fos = new FileOutputStream(new File(".//uploadFiles//" + uploadFileName
+            fis = (FileInputStream) multiReq.getFile("file").getInputStream();
+            fos = new FileOutputStream(new File(  "uploadFiles/"+uploadFileName
                     + ".")
                     + uploadFileSuffix);
             byte[] temp = new byte[1024];
@@ -63,5 +71,7 @@ public class FileController {
                 }
             }
         }
+        return ResultFactory.buildSuccessResult(uploadFileName+uploadFileSuffix);
+
     }
 }
